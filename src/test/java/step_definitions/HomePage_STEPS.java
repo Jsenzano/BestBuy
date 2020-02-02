a0f1865663b5f472508c6f43c99196961d79d44f
unknown <darchievboris@gmail.com>
2020-02-02 15:23:06 -0500

    screen02 changes

--- src/test/java/step_definitions/HomePage_STEPS.java
+++ src/test/java/step_definitions/HomePage_STEPS.java
@@ -1,5 +1,6 @@
 package step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,6 +8,8 @@ import org.junit.Assert;
 import step_definitions.step_impl.HomePage_impl;
 import util.ConfigReader;
 import util.Driver;
+import util.Screenshots;
+import util.SeleniumUtils;
 
 public class HomePage_STEPS {
     HomePage_impl homePage_impl = new HomePage_impl();
@@ -18,22 +21,42 @@ public class HomePage_STEPS {
 
     @Then("Verifies title is {string}")
     public void verifies_title_is(String string) {
-        Assert.assertEquals("Best Buy | Official Online Store | Shop Now & Save",homePage_impl.getTitle());
+        Assert.assertEquals("Best Buy | Official Online Store | Shop Now & Save", homePage_impl.getTitle());
     }
 
     @When("User sees {string}")
     public void user_sees(String string) {
         homePage_impl.clickCloseModalSignUp();
-        Assert.assertEquals("Today's popular picks",homePage_impl.getPopularPicksName());
+        Assert.assertEquals("Today's popular picks", homePage_impl.getPopularPicksName());
     }
 
     @When("User navigates to {string}")
-    public void user_navigates_to(String navItem){
+    public void user_navigates_to(String navItem) {
         try {
             homePage_impl.clickCloseModalSignUp();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         homePage_impl.clickNavItem(navItem);
     }
+
+    @Then("Verifies bottom navigation name with following info")
+    public void verifies_bottom_navigation_name_with_following_info(io.cucumber.datatable.DataTable dataTable) {
+//        Assert.assertEquals(bookingPageImpl.expectedCouponMessage, bookingPageImpl.getCouponCodeConfirmationText());
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new cucumber.api.PendingException();
+    }
+
+    @And("Takes screenshot")
+    public void Takes_screenshot() {
+        SeleniumUtils.sleep(1000);
+        Screenshots.captureScreenShot();
+    }
+
 }
